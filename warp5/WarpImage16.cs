f3cf269e28b3174c98edbb686bace5bb25169fd2
Ron Smith <spinlock663@gmail.com>
2019-03-20 17:21:14 -0700

    Added -,* operators

--- warp5/WarpImage16.cs
+++ warp5/WarpImage16.cs
@@ -73,5 +73,104 @@ namespace warp5
                 }
             return new WarpImage16(nWidth, nHeight, DTYPE.INT16, a.OName, a.Notes,a.Ra,a.Dec, nData);
         }
+        public static WarpImage16 operator -(WarpImage16 a, WarpImage16 b)
+        {
+            uint nWidth;
+            uint nHeight;
+            uint lWidth;
+            uint lHeight;
+            ushort[,] nData;
+            bool aMajor = true;
+            if (a.Width >= b.Width && a.Height >= b.Height)
+            {
+                nWidth = a.Width;
+                nHeight = a.Height;
+                lWidth = b.Width;
+                lHeight = b.Height;
+                aMajor = true;
+            }
+            else if (a.Width >= b.Width && a.Height >= b.Height)
+            {
+                nWidth = b.Width;
+                nHeight = b.Height;
+                lWidth = a.Width;
+                lHeight = a.Height;
+                aMajor = false;
+            }
+            else
+            {
+                throw new ArithmeticException("Error: Dim Missmatch");
+            }
+            nData = new ushort[nWidth, nHeight];
+            for (uint i = 0; i < nHeight; i++)
+                for (uint j = 0; j < nWidth; j++)
+                {
+                    if (aMajor)
+                    {
+                        if (i < lHeight && j < lWidth)
+                            nData[i, j] = (ushort)(a.getData(i, j) - b.getData(i, j));
+                        else
+                            nData[i, j] = a.getData(i, j);
+                    }
+                    else
+                    {
+                        if (i < lHeight && j < lWidth)
+                            nData[i, j] = (ushort)(a.getData(i, j) - b.getData(i, j));
+                        else
+                            nData[i, j] = b.getData(i, j);
+                    }
+                }
+            return new WarpImage16(nWidth, nHeight, DTYPE.INT16, a.OName, a.Notes, a.Ra, a.Dec, nData);
+        }
+        public static WarpImage16 operator *(WarpImage16 a, WarpImage16 b)
+        {
+            uint nWidth;
+            uint nHeight;
+            uint lWidth;
+            uint lHeight;
+            ushort[,] nData;
+            bool aMajor = true;
+            if (a.Width >= b.Width && a.Height >= b.Height)
+            {
+                nWidth = a.Width;
+                nHeight = a.Height;
+                lWidth = b.Width;
+                lHeight = b.Height;
+                aMajor = true;
+            }
+            else if (a.Width >= b.Width && a.Height >= b.Height)
+            {
+                nWidth = b.Width;
+                nHeight = b.Height;
+                lWidth = a.Width;
+                lHeight = a.Height;
+                aMajor = false;
+            }
+            else
+            {
+                throw new ArithmeticException("Error: Dim Missmatch");
+            }
+            nData = new ushort[nWidth, nHeight];
+            for (uint i = 0; i < nHeight; i++)
+                for (uint j = 0; j < nWidth; j++)
+                {
+                    if (aMajor)
+                    {
+                        if (i < lHeight && j < lWidth)
+                            nData[i, j] = (ushort)(a.getData(i, j) * b.getData(i, j));
+                        else
+                            nData[i, j] = a.getData(i, j);
+                    }
+                    else
+                    {
+                        if (i < lHeight && j < lWidth)
+                            nData[i, j] = (ushort)(a.getData(i, j) * b.getData(i, j));
+                        else
+                            nData[i, j] = b.getData(i, j);
+                    }
+                }
+            return new WarpImage16(nWidth, nHeight, DTYPE.INT16, a.OName, a.Notes, a.Ra, a.Dec, nData);
+        }
+       
     }
 }
